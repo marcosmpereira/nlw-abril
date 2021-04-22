72637a8e88ea73ac89a36887af9059efa4c22078
Marcos Moraes Pereira <marcosmpereira1992@gmail.com>
2021-04-21 21:06:36 -0300

    [UPDATE] - Adicionado tipagem no retorno dos dados do endpoit de podcasts e adicionado tipagem na props do componente Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
+import { GetStaticProps } from 'next';
 
-export default function Home(props) {
+type File = {
+  url: string;
+  type: string;
+  duration: number
+}
+
+type Episodes = {
+  id: string;
+  title: string;
+  members: string;
+  published_at: Date;
+  thumbnail: string;
+  description: string
+  file: File
+}
+
+type HomeProps = {
+  episodes: Array<Episodes>
+}
+
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>App</h1>
@@ -8,14 +30,14 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
-	const response = await fetch('http://localhost:3333/episodes')
+export const getStaticProps: GetStaticProps = async () => {
+	const response = await fetch('http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc')
 	const data = await response.json()
 
   return {
 		props: {
 			episodes: data
 		},
-		revalidate: 60 * 60 * 8, // define quanto tempo em segundos os dados serão atualizados
+		revalidate: 60 * 60 * 8
 	}
 }
